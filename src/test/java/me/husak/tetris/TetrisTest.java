2c90ebc04e733e032f6e61755b83a4020475279f
Krunoslav Husak <kruno@husak.me>
2023-04-06 21:03:45 +0200

    Created Maven project

---
+++ src/test/java/me/husak/tetris/TetrisTest.java
@@ -0,0 +1,14 @@
+package me.husak.tetris;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TetrisTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
